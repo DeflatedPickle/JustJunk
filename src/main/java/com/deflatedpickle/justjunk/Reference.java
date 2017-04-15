15b5b3e3aa3597be3615329571e6295e70a5ed3c
Dibbo <DeflatedPickle@gmail.com>
2017-04-15 23:01:15 +0100

    The Tin Can can now be opened for trash loot

--- src/main/java/com/deflatedpickle/justjunk/Reference.java
+++ src/main/java/com/deflatedpickle/justjunk/Reference.java
@@ -3,7 +3,7 @@ package com.deflatedpickle.justjunk;
 public class Reference {
     public static final String MOD_ID = "dpjjm";
     public static final String NAME = "JustJunk";
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.8.0";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
     public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.justjunk.proxy.ClientProxy";
--- src/main/java/com/deflatedpickle/justjunk/items/ItemTincan.java
+++ src/main/java/com/deflatedpickle/justjunk/items/ItemTincan.java
@@ -2,13 +2,40 @@ package com.deflatedpickle.justjunk.items;
 
 import com.deflatedpickle.justjunk.JustJunk;
 import com.deflatedpickle.justjunk.Reference;
+import com.deflatedpickle.justjunk.init.ModItems;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class ItemTincan extends Item{
     public ItemTincan(){
         setUnlocalizedName(Reference.JustJunkItems.TINCAN.getUnlocalizedName());
         setRegistryName(Reference.JustJunkItems.TINCAN.getRegistryName());
         setMaxStackSize(1);
+        setMaxDamage(1);
         setCreativeTab(JustJunk.tabJustJunk);
+    }
+
+    private static final Item[] loot_table = new Item[] {
+            Items.PAPER,
+            Items.ROTTEN_FLESH,
+            Items.RABBIT,
+            Items.RECORD_CAT,
+
+            ModItems.rottingfish,
+    };
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn){
+        playerIn.inventory.addItemStackToInventory(new ItemStack(ModItems.emptytincan,1));
+        playerIn.addExperience(1);
+        playerIn.inventory.deleteStack(playerIn.getHeldItem(handIn));
+        ItemStack random_item = new ItemStack(loot_table[(int) (Math.random() * loot_table.length)], 1);
+        playerIn.inventory.addItemStackToInventory(random_item);
+        return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
