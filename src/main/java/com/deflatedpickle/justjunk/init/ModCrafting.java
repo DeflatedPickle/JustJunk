738c67c5958aad06d3866b5ab1a315f7db06f458
Dibbo <DeflatedPickle@gmail.com>
2017-04-21 16:12:33 +0100

    Fixed Tinfoil Hat recipe

--- src/main/java/com/deflatedpickle/justjunk/init/ModCrafting.java
+++ src/main/java/com/deflatedpickle/justjunk/init/ModCrafting.java
@@ -2,6 +2,7 @@ package com.deflatedpickle.justjunk.init;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModCrafting {
@@ -12,7 +13,9 @@ public class ModCrafting {
         GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.tinblock), "TTT", "TTT", "TTT", 'T', ModItems.tiningot);
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tiningot, 9), ModBlocks.tinblock);
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tinfoil, 2), ModItems.tiningot);
-        GameRegistry.addShapedRecipe(new ItemStack(ModItems.tiningot), "TTT", "TXT", "XXX", 'T', ModItems.tinfoil);
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.tinfoilhelmet), "TTT", "TXT", "XXX", 'T', ModItems.tinfoil);
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.toothbrushshiv), ModItems.toothbrush, Items.FLINT);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tiningot), ModItems.tinfoil, ModItems.tinfoil, ModItems.tinfoil, ModItems.tinfoil);
+        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.tiningot, 9), ModBlocks.tinblock);
     }
 }
